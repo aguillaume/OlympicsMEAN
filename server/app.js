4373f88667bea70933ebfa3da03fe9b4675a17d1
Guillaume Alpe <guillaumealpe@hotmail.com>
2016-03-12 20:14:21 +0000

    add express. Serve static file and add 1st endpoint

---
+++ server/app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let express = require("express");
+let app = express();
+
+app.use( express.static(__dirname + "/../client") ); //tells express to look inside client. This will serve the index.html file.
+app.get("/sports", (request, response) => {
+  response.json(["Cycling", "Weightlisfting", "Swimming"]);
+});
+app.listen(8181, () => console.log("listening on 8181"));
